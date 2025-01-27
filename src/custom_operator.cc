23f6ce3da40a6ac9da2f6da66710aad761a8aa3d
Daniel Sanchez <danielsanchezaran@gmail.com>
2025-01-27 18:20:24 +0900

    wip following this guide https://cassiebreviu.github.io/onnxruntime/docs/reference/operators/add-custom-op.html
    Signed-off-by: Daniel Sanchez <danielsanchezaran@gmail.com>

--- src/custom_operator.cc
+++ src/custom_operator.cc
@@ -1,28 +1,78 @@
+// custom_op_library.cc
+
+#include "custom_operator.h"
+
+// Custom operator libraries are not typically linked with ONNX Runtime.
+// Therefore, must define ORT_API_MANUAL_INIT before including
+// onnxruntime_cxx_api.h to indicate that the OrtApi object will be initialized
+// manually.
 #define ORT_API_MANUAL_INIT
 #include "onnxruntime_cxx_api.h"
 #undef ORT_API_MANUAL_INIT
 
-#include "onnxruntime_lite_custom_op.h"
-
 #include <cmath>
 #include <mutex>
-#include <system_error>
 #include <vector>
 
-void KernelOne(const Ort::Custom::Tensor<float> &X,
-               const Ort::Custom::Tensor<float> &Y,
-               Ort::Custom::Tensor<float> &Z) {
-  auto input_shape = X.Shape();
-  auto x_raw = X.Data();
-  auto y_raw = Y.Data();
-  auto z_raw = Z.Allocate(input_shape);
-  for (int64_t i = 0; i < Z.NumberOfElement(); ++i) {
-    z_raw[i] = x_raw[i] + y_raw[i];
+struct MyCustomKernel {
+  MyCustomKernel(const OrtApi &api, const OrtKernelInfo *info) {}
+
+  void Compute(OrtKernelContext *context) {
+    // Setup inputs
+    Ort::KernelContext ctx(context);
+    Ort::ConstValue input_X = ctx.GetInput(0);
+    Ort::ConstValue input_Y = ctx.GetInput(1);
+    const float *X = input_X.GetTensorData<float>();
+    const float *Y = input_Y.GetTensorData<float>();
+
+    // Setup output, which is assumed to have the same dimensions as the inputs.
+    std::vector<int64_t> dimensions =
+        input_X.GetTensorTypeAndShapeInfo().GetShape();
+
+    Ort::UnownedValue output = ctx.GetOutput(0, dimensions);
+    float *out = output.GetTensorMutableData<float>();
+
+    const size_t size = output.GetTensorTypeAndShapeInfo().GetElementCount();
+
+    // Do computation
+    for (size_t i = 0; i < size; i++) {
+      out[i] = X[i] + Y[i];
+    }
   }
-}
+};
 
-static const char *c_OpDomain = "test.customop";
+struct MyCustomOp : Ort::CustomOpBase<MyCustomOp, MyCustomKernel> {
+  void *CreateKernel(const OrtApi &api, const OrtKernelInfo *info) const {
+    return std::make_unique<MyCustomKernel>(api, info).release();
+  };
 
+  // Returns the name of the custom operator.
+  const char *GetName() const { return "MyCustomOp"; };
+
+  // Returns the custom operator's execution provider.
+  const char *GetExecutionProviderType() const {
+    return "CPUExecutionProvider";
+  };
+
+  // Returns the number of inputs.
+  size_t GetInputTypeCount() const { return 2; };
+
+  // Returns the type of each input. Both inputs are tensor(float).
+  ONNXTensorElementDataType GetInputType(size_t index) const {
+    return ONNX_TENSOR_ELEMENT_DATA_TYPE_FLOAT;
+  };
+
+  // Returns the number of outputs.
+  size_t GetOutputTypeCount() const { return 1; };
+
+  // Returns the type of each output. The single output is a tensor(float).
+  ONNXTensorElementDataType GetOutputType(size_t index) const {
+    return ONNX_TENSOR_ELEMENT_DATA_TYPE_FLOAT;
+  };
+};
+
+// This function shows one way of keeping domains alive until the library is
+// unloaded.
 static void AddOrtCustomOpDomainToContainer(Ort::CustomOpDomain &&domain) {
   static std::vector<Ort::CustomOpDomain> ort_custom_op_domain_container;
   static std::mutex ort_custom_op_domain_mutex;
@@ -30,33 +80,30 @@ static void AddOrtCustomOpDomainToContainer(Ort::CustomOpDomain &&domain) {
   ort_custom_op_domain_container.push_back(std::move(domain));
 }
 
+// Called by ONNX Runtime to register the library's custom operators with the
+// provided session options.
 OrtStatus *ORT_API_CALL RegisterCustomOps(OrtSessionOptions *options,
                                           const OrtApiBase *api) {
+  Ort::InitApi(
+      api->GetApi(ORT_API_VERSION)); // Manually initialize the OrtApi to enable
+                                     // use of C++ API classes and functions.
 
-  Ort::CustomOpDomain domain{c_OpDomain};
-  Ort::CustomOpDomain domain_v2{"v2"};
+  // Custom operators are static to ensure they remain valid until the library
+  // is unloaded.
+  static const MyCustomOp my_custom_op;
 
-  Ort::UnownedSessionOptions session_options(options);
-  session_options.Add(domain);
-  session_options.Add(domain_v2);
-  AddOrtCustomOpDomainToContainer(std::move(domain));
-  AddOrtCustomOpDomainToContainer(std::move(domain_v2));
-}
+  OrtStatus *result = nullptr;
 
-OrtStatus *ORT_API_CALL RegisterCustomOpsAltName(OrtSessionOptions *options,
-                                                 const OrtApiBase *api) {
-  return RegisterCustomOps(options, api);
-}
+  try {
+    Ort::CustomOpDomain domain{"awml_pred"};
+    domain.Add(&my_custom_op);
 
-int main() {
-  Ort::CustomOpDomain v1_domain{"v1"};
-  // please make sure that custom_op_one has the same lifetime as the consuming
-  // session
-  std::unique_ptr<Ort::Custom::OrtLiteCustomOp> custom_op_one{
-      Ort::Custom::CreateLiteCustomOp("CustomOpOne", "CPUExecutionProvider",
-                                      KernelOne)};
-  v1_domain.Add(custom_op_one.get());
-  Ort::SessionOptions session_options;
-  session_options.Add(v1_domain);
-  // create a session with the session_options ...
+    Ort::UnownedSessionOptions session_options(options);
+    session_options.Add(domain);
+    AddOrtCustomOpDomainToContainer(std::move(domain));
+  } catch (const std::exception &e) {
+    Ort::Status status{e};
+    result = status.release();
+  }
+  return result;
 }
--- src/custom_operator.h
+++ src/custom_operator.h
@@ -1,16 +1,15 @@
+// custom_op_library.h
+
 #pragma once
-#include "onnxruntime_c_api.h"
+
+#include <onnxruntime_c_api.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-ORT_EXPORT OrtStatus *ORT_API_CALL RegisterCustomOps(OrtSessionOptions *options,
-                                                     const OrtApiBase *api);
-
-// alternative name to test registration by function name
 ORT_EXPORT OrtStatus *ORT_API_CALL
-RegisterCustomOpsAltName(OrtSessionOptions *options, const OrtApiBase *api);
+RegisterCustomOps(OrtSessionOptions *options, const OrtApiBase *api_base);
 
 #ifdef __cplusplus
 }
