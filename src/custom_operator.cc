101f645e7b34664198b1086917498a924f085a93
Daniel Sanchez <danielsanchezaran@gmail.com>
2025-01-28 09:25:50 +0900

    change namespace and name
    Signed-off-by: Daniel Sanchez <danielsanchezaran@gmail.com>

--- src/custom_operator.cc
+++ src/custom_operator.cc
@@ -47,11 +47,11 @@ struct MyCustomOp : Ort::CustomOpBase<MyCustomOp, MyCustomKernel> {
   };
 
   // Returns the name of the custom operator.
-  const char *GetName() const { return "awml_pred:TRTKnnBatchMlogK(-1)"; };
+  const char *GetName() const { return "TRTKnnBatchMlogK"; };
 
   // Returns the custom operator's execution provider.
   const char *GetExecutionProviderType() const {
-    return "GPUExecutionProvider";
+    return "CPUExecutionProvider";
   };
 
   // Returns the number of inputs.
